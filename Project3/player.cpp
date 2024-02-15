dbd80456e7d5d9c9e8abf86aa908935a7afc48c3
Dark <63397578+darkexitmatrix@users.noreply.github.com>
2024-02-15 15:36:07 +0100

    Update player.cpp

--- Project3/player.cpp
+++ Project3/player.cpp
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-// player.cpp
 #include "player.h"
 #include "character.h"
 using namespace std;
@@ -23,13 +21,12 @@ void Player::setRole(Role* role) {
 
 int Player::getPlayerHealth() const {
     if (character != nullptr) {
-        return character->getCharacterHealth(); 
+        return character->getCharacterHealth();
     }
     else {
         return 0;
     }
 }
-
 
 void Player::setPlayerHealth(int hp) {
     healthPoints = hp;
@@ -46,43 +43,7 @@ std::string Player::getPlayerName() const {
 Character* Player::getPlayerCharacter() const {
     return character;
 }
+
 Role* Player::getRole() const {
     return role;
 }
-=======
-    #include "player.h"
-
-    Player::Player() : role(nullptr), healthPoints(0) {
-    }
-
-    Player::Player(const std::string& name) : name(name), role(nullptr), healthPoints(0) {
-    }
-
-    void Player::setName(const std::string& name) {
-        this->name = name;
-    }
-
-    void Player::setRole(Role* role) {
-        this->role = role;
-    }
-
-    int Player::getHealthPoints() const {
-        return healthPoints;
-    }
-
-    void Player::setHealthPoints(int hp) {
-        healthPoints = hp;
-    }
-
-    bool Player::isDefeated() const {
-        return healthPoints <= 0;
-    }
-
-    std::string Player::getName() const {
-        return name;
-    }
-
-    Role* Player::getRole() const {
-        return role;
-    }
->>>>>>> 1ca5599a4e6e1ae52d94a8f163e67fe4dc027e8f
